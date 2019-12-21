b839a8912a80ba4bf53f14a7b29d0b9d4a7b1f89
Michelle Morin <morin2m@gmail.com>
2019-12-21 01:49:27 -0500

    update logic for replacing numbers with phrases

--- js/scripts.js
+++ js/scripts.js
@@ -13,10 +13,10 @@ $(document).ready(function() {
     var listOfNumbers = [];
       for (var i = 0; i <= parseInt(inputNumber); i ++) {
         listOfNumbers.push(i);
-        console.log(listOfNumbers);
       }
+    console.log(listOfNumbers);
 
-    listOfNumbers.forEach(function(number) {
+    listOfNumbers.toString().forEach(function(number) {
       if (number.indexOf('3') > -1) {
         number = "I'm sorry, Dave. I can't do that.";
       } else if (number.indexOf('2') > -1) {
@@ -27,8 +27,8 @@ $(document).ready(function() {
     });
 
 
-    listOfNumbers.forEach(function(number) {
-      $("ul#result").append("<li>" + number + "</li>");
+    listOfNumbers.forEach(function(element) {
+      $("ul#result").append("<li>" + element + "</li>");
     })
   });
 });
